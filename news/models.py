dd1cf69f88792b1d0da2f247fb47f01dd8f131bd
maninserg <manin.serg@gmail.com>
2021-01-28 17:53:32 +0300

    Create migration and database

--- news/models.py
+++ news/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 class News(models.Model):
-    title =  models.CharField(max_lenght=150)
+    title = models.CharField(max_length=150)
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
