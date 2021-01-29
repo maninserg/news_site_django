806305cfe12daa90e464eb4a9b87988a0413f616
maninserg <manin.serg@gmail.com>
2021-01-29 15:07:24 +0300

    #11 Create templates

--- news/views.py
+++ news/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
+
 
 def index(request):
-    print(request)
-    return HttpResponse('Hello, World!')
-
-def test(request):
-    return HttpResponse('<h1> Тестовая страница </h1>')
+    news = News.objects.order_by('-created_at')
+    context = {'news': news, 'title': 'Cписок новостей'}
+    return render(request, 'news/index.html', context)
