c8fd5be64ee8519e79de5b7d351baed2de0f0f91
maninserg <manin.serg@gmail.com>
2021-02-01 11:54:51 +0300

    #19 Add template base.html

--- news_site/settings.py
+++ news_site/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'news_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
