6e3db478644f310262098e15bfa46b65d2b226a1
maninserg <manin.serg@gmail.com>
2021-04-23 20:24:20 +0300

    #25 Change form to form is made by model

--- news/views.py
+++ news/views.py
@@ -25,8 +25,7 @@ def add_news(request):
     if request.method == 'POST':
         form = NewsForm(request.POST)
         if form.is_valid():
-            # print(form.cleaned_data)
-            news = News.objects.create(**form.cleaned_data)
+            news = form.save()
             return redirect(news.get_absolute_url())
     else:
         form = NewsForm()
