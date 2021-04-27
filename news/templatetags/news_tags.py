2620e1e146d265622f1e71f8f4da096ac8e879c8
maninserg <manin.serg@gmail.com>
2021-04-27 22:12:31 +0300

    #51 Add filesystem caching

--- news/templatetags/news_tags.py
+++ news/templatetags/news_tags.py
@@ -1,6 +1,7 @@
 from django import template
 from django.db.models import Count, F
 from news.models import Category, News
+from django.core.cache import cache
 
 
 register = template.Library()
@@ -14,7 +15,10 @@ def get_categories():
 def show_categories():
 #    categories = Category.objects.all()
 #    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt= 0)
-    categories = Category.objects.annotate(
-        cnt=Count('news',
-                  filter=F('news__is_published'))).filter(cnt__gt= 0)
+    categories = cache.get('categories')
+    if not categories:
+        categories = Category.objects.annotate(
+            cnt=Count('news',
+                    filter=F('news__is_published'))).filter(cnt__gt= 0)
+        cache.set('categories', categories, 30)
     return {'categories': categories}
--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     path('register/', register, name='register'),
@@ -12,5 +13,6 @@ urlpatterns = [
     path('news/<int:pk>/', ViewNews.as_view(), name='view_news'),
 #    path('news/add_news', add_news, name='add_news'),
     path('news/add_news/', CreateNews.as_view(), name='add_news'),
+#    path('', cache_page(60)(HomeNews.as_view()), name='home'),
     path('', HomeNews.as_view(), name='home'),
 ]
