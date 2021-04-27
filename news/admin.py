9fc7dc72229a60e699b02117cd6b295006c79c23
maninserg <manin.serg@gmail.com>
2021-04-27 21:29:59 +0300

    #49 Add CKEditor for django admin

--- news/admin.py
+++ news/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 from .models import News, Category
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django.utils.safestring import mark_safe
+from django import forms
+
+
+class NewsAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = News
+        fields = '__all__'
 
 
 class NewsAdmin(admin.ModelAdmin):
+    form = NewsAdminForm
     list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published',
                     'category', 'get_photo')
     list_display_links = ('id', 'title')
